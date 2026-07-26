94e13c9b55cad0b16962645b39c7ef4872182d2b
Drak3421 <namanka29087@gmail.com>
2026-07-27 01:27:44 +0530

    Fix duplicate music toggle and dynamically update landing page stats

---
+++ update_home_stats.cjs
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, 'public', 'dpal', 'data.js');
+const homePath = path.join(__dirname, 'public', 'dpal', 'home.html');
+
+try {
+    const dataContent = fs.readFileSync(dataPath, 'utf8');
+    const startIndex = dataContent.indexOf('[');
+    const arrayStr = dataContent.slice(startIndex).replace(/;$/, '');
+    const data = JSON.parse(arrayStr);
+
+    let homeHtml = fs.readFileSync(homePath, 'utf8');
+    let totalResources = 0;
+    
+    const catMap = {};
+    for (const cat of data) {
+        let catTotal = 0;
+        if (cat.subcategories) {
+            for (const sub of cat.subcategories) {
+                catTotal += sub.items.length;
+            }
+        }
+        catMap[cat.name] = catTotal;
+        if (cat.name !== "Newly Added Websites") {
+            totalResources += catTotal;
+        }
+    }
+    
+    // Update main hero stat
+    const formattedTotal = new Intl.NumberFormat('en-US').format(totalResources);
+    homeHtml = homeHtml.replace(/<span>[\d,]+\+? Verified Resources<\/span>/g, `<span>${formattedTotal}+ Verified Resources</span>`);
+    
+    // Update individual pills using a reliable regex
+    // Movies / TV / Anime
+    if (catMap['Movies / TV / Anime'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Movies.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Movies / TV / Anime']} topics$2`);
+    }
+    if (catMap['Artificial Intelligence'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Artificial.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Artificial Intelligence']} topics$2`);
+    }
+    if (catMap['Adblocking & Privacy'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Adblocking.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Adblocking & Privacy']} topics$2`);
+    }
+    if (catMap['Music & Audio'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Music.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Music & Audio']} topics$2`);
+    }
+    if (catMap['Books & Reading'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Books.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Books & Reading']} topics$2`);
+    }
+    if (catMap['Gaming & Emulation'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Gaming.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Gaming & Emulation']} topics$2`);
+    }
+    if (catMap['Android & iOS'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Android.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Android & iOS']} topics$2`);
+    }
+    if (catMap['Developer Tools'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Developer.*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Developer Tools']} topics$2`);
+    }
+    if (catMap['Torrenting'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Torrenting".*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Torrenting']} topics$2`);
+    }
+    if (catMap['Educational'] !== undefined) {
+        homeHtml = homeHtml.replace(/(href="\.\/index\.html#Educational".*?<div class="topic-pill">)[\d,]+ topics(<\/div>)/s, `$1${catMap['Educational']} topics$2`);
+    }
+    
+    fs.writeFileSync(homePath, homeHtml, 'utf8');
+    console.log("Successfully updated home.html stats with total:", formattedTotal);
+
+} catch (err) {
+    console.error("Failed to update home stats:", err);
+}
